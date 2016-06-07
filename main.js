b408bae987c3c75f2d8a5d954bf0809db01e773f
Jacob Seibert <12jtseibert@gmail.com>
2016-06-07 13:01:14 -0500

    test

--- main.js
+++ main.js
@@ -22,6 +22,6 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
