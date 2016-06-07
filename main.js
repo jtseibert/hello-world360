041963c2850603483f6ee82e401d73f141f42965
Sheldon Tan <sheldon.tan@magnet360.com>
2016-06-07 12:41:57 -0500

    changing a lot

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
-var express = require('express');
-var app = express();
+// Load the http module to create an http server.
+var http = require('http');
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+// Configure our HTTP server to respond with Hello World to all requests.
+var server = http.createServer(function (request, response) {
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.end("Hello World\n");
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-});
+// Listen on port 8000, IP defaults to 127.0.0.1
+server.listen(8000);
+
+// Put a friendly message on the terminal
+console.log("Server running at http://127.0.0.1:8000/");
