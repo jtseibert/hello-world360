78cfe0497ad8ae260399e681cb40aedb8f18c762
Sheldon Tan <sheldon.tan@magnet360.com>
2016-06-08 16:07:34 -0500

    modify main.js

--- main.js
+++ main.js
@@ -1,15 +1,4 @@
-var express = require('express');
-var $ = require('jquery');
-
-
-var app = express();
-
-app.set('port', (process.env.PORT || 5000));
-
-app.get('/', function (req, res) {
-	res.send('Hello World!');
-
-	/**************** Begin REST API attempt **********************************/
+var jquery = require('jquery');
 
 	var access_token;
 	var instance_url;
@@ -61,12 +50,5 @@ app.get('/', function (req, res) {
 		} 
 	});
 	};
+};
 
-	/********************** End REST API attempt *****************************/
-	res.send('Hello World!');
-
-});
-
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
