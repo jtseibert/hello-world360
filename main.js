e9d51f0b2acc4f002ff78f4a5b8f88efdb08e4d4
Jacob Seibert <12jtseibert@gmail.com>
2016-06-08 15:00:12 -0500

    test

--- main.js
+++ main.js
@@ -7,62 +7,63 @@ var app = express();
 app.set('port', (process.env.PORT || 5000));
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+	res.send('Hello World!');
 
-/**************** Begin REST API attempt **********************************/
+	/**************** Begin REST API attempt **********************************/
 
-var access_token;
-var instance_url;
-var id;
-var issued_at;
-var signature;
+	var access_token;
+	var instance_url;
+	var id;
+	var issued_at;
+	var signature;
 
-function authenticate(clientID, clientSecret, username, password){
+	function authenticate(clientID, clientSecret, username, password){
 
-	var postURL = "grant_type=password&client_id=" + clientID
-					+ "&client_secret=" + clientSecret
-					+ "&username=" + username
-					+ "&password=" + password;
-var test = function($){
-    $.ajax({
-        url: "https://login.salesforce.com/services/oauth2/token",
-        type: "POST",
-        data: postURL,
-        success: function(responseData){
-        	access_token = responseData.access_token;
-        	instance_url = responseData.instance_url;
-        	id = responseData.id;
-        	issued_at = responseData.issued_at;
-        	signature = responseData.signature;
-        }
+		var postURL = "grant_type=password&client_id=" + clientID
+						+ "&client_secret=" + clientSecret
+						+ "&username=" + username
+						+ "&password=" + password;
+	var test = function($){
+	    $.ajax({
+	        url: "https://login.salesforce.com/services/oauth2/token",
+	        type: "POST",
+	        data: postURL,
+	        success: function(responseData){
+	        	access_token = responseData.access_token;
+	        	instance_url = responseData.instance_url;
+	        	id = responseData.id;
+	        	issued_at = responseData.issued_at;
+	        	signature = responseData.signature;
+	        }
+		});
+	};
+	}
+
+	var initParameters = {
+		clientID: "MVG9uudbyLbNPZOEM.vAy8Y1H8RF8ocpnP1nW2Nt_2a9aFFOjolOIyKa6.1QCCfC9ZreHWPMWEIJhSnQuQqP",
+		clientSecret: "4299800700281945236",
+		username: "jacobseibert@magnet360.com",
+		password: "Zedc3093"
+	};
+
+	authenticate(initParameters.clientID,
+					initParameters.clientSecret,
+					initParameters.username,
+					initParameters.password);
+	var testt = function($){
+	$.ajax({
+		url: instance_url,
+		type: "POST",
+		data: "services/data/",
+		authorization: "Bearer " + access_token,
+		success: function(responseData){
+			res.send(responseData.stringify());
+		} 
 	});
-};
-}
+	};
 
-var initParameters = {
-	clientID: "MVG9uudbyLbNPZOEM.vAy8Y1H8RF8ocpnP1nW2Nt_2a9aFFOjolOIyKa6.1QCCfC9ZreHWPMWEIJhSnQuQqP",
-	clientSecret: "4299800700281945236",
-	username: "jacobseibert@magnet360.com",
-	password: "Zedc3093"
-};
-
-authenticate(initParameters.clientID,
-				initParameters.clientSecret,
-				initParameters.username,
-				initParameters.password);
-var testt = function($){
-$.ajax({
-	url: instance_url,
-	type: "POST",
-	data: "services/data/",
-	authorization: "Bearer " + access_token,
-	success: function(responseData){
-		res.send(responseData.stringify());
-	} 
-});
-};
-
-/********************** End REST API attempt *****************************/
+	/********************** End REST API attempt *****************************/
+	res.send('Hello World!');
 
 });
 
