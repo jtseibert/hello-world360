21dccfbc6a43173edc4eb21787a778f601e7f70b
Sheldon Tan <sheldon.tan@magnet360.com>
2016-06-08 10:31:47 -0500

    added jquery and authenticate

---
+++ authenticate.js
@@ -0,0 +1,13 @@
+class Authenticate {
+	constructor(clientID, clientSecret, redirectUri, environment) {
+		//param error checking?
+
+		this[_clientID] = clientID;
+		this[_clientSecret] = clientSecret;
+		this[_redirectUri] = redirectUri;
+		this[_environement] = environment;
+	}
+
+
+
+}
