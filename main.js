3f7252789b74d716524267c25143ed9ab5441371
Jacob Seibert <12jtseibert@gmail.com>
2016-06-08 15:19:45 -0500

    test

--- main.js
+++ main.js
@@ -13,6 +13,7 @@ app.get('/', function (req, res) {
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
   console.log('begin authenticate');
+  res.send('begin!\n');
 
   /**************** Begin REST API attempt **********************************/
 
