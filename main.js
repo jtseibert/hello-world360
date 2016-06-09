5c8829919cdd1c0d5c4a7e0fbd9c235dc9ff5414
Jacob Seibert <12jtseibert@gmail.com>
2016-06-09 09:51:15 -0500

    test

--- main.js
+++ main.js
@@ -88,12 +88,6 @@ jsdom.jQueryify(window, "http://code.jquery.com/jquery.js", function () {
 
   	$("<h1>test passes</h1>").appendTo("body");
 	console.log($("body").html());
-
-	jq("<h2>other test passes</h2>").appendTo("body");
-	console.log(jq("body").html());
-
-	jQuery("<h3>third test passes</h3>").appendTo("body");
-	console.log(jQuery("body").html());
 });
 
 
