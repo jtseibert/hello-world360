ec5ea44bf0b1bb56060dc84ee4ebbe00c353cff9
Jacob Seibert <12jtseibert@gmail.com>
2016-06-10 09:59:38 -0500

    updted listen statment

--- src/authenticate.js
+++ src/authenticate.js
@@ -49,8 +49,8 @@ app.get('/', function (req, res) {
   res.render('main');
 });
 
-.listen(process.env.PORT || 5000);
+app.listen(process.env.PORT || 5000);
 
-console.log('Express server started on port 3000');
+console.log('Express server started on port 5000');
 
 
