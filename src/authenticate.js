1d6050d4f0813b0d519e402eeaac3c1fcc6f3796
Jacob Seibert <12jtseibert@gmail.com>
2016-06-10 16:46:54 -0500

    test

--- src/authenticate.js
+++ src/authenticate.js
@@ -32,7 +32,7 @@ var options = {
     path: '/services/data/v36.0/analytics/reports/00O36000005vYLW/',
     method: 'GET',
     headers: {
-        'Authorization': access_token,
+        'Authorization': 'Bearer ' + access_token,
         'Content-Type': 'application/json'
     }
 };
