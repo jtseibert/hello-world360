54a9b4b1e9f2583b7ee12f47a669abd7e808a090
Jacob Seibert <12jtseibert@gmail.com>
2016-06-10 23:27:34 -0500

    test

--- src/authenticate.js
+++ src/authenticate.js
@@ -69,7 +69,7 @@ var oauth2 = oauth2(credentials);
 
 // Authorization uri definition
 var authorization_uri = oauth2.authCode.authorizeURL({
-        redirect_uri: 'http://localhost:5000/callback',
+        redirect_uri: 'https://hello-world360.herokuapp.com/callback',
         scope: 'full'
     });
 
