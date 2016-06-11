4f356b4071d314025b89e6bbc1a8a36f674d7437
Jacob Seibert <12jtseibert@gmail.com>
2016-06-10 21:17:46 -0500

    cleaned up file and checked missing args

--- src/authenticate.js
+++ src/authenticate.js
@@ -1,53 +1,109 @@
-// var url = 'https://login.salesforce.com/services/oauth2/token',
-//     clientID = "MVG9uudbyLbNPZOEM.vAy8Y1H8RF8ocpnP1nW2Nt_2a9aFFOjolOIyKa6.1QCCfC9ZreHWPMWEIJhSnQuQqP",
-//     clientSecret = "4299800700281945236",
-//     username = "jacobseibert@magnet360.com",
-//     password = "Zedc3093";
 
+/**************** Just for reference *************
+var url = 'https://login.salesforce.com/services/oauth2/token',
+    clientID = "MVG9uudbyLbNPZOEM.vAy8Y1H8RF8ocpnP1nW2Nt_2a9aFFOjolOIyKa6.1QCCfC9ZreHWPMWEIJhSnQuQqP",
+    clientSecret = "4299800700281945236",
+    username = "jacobseibert@magnet360.com",
+    password = "Zedc3093";
+*************************************************/
+
+
+
+
+
+/******************* Requires *******************/
 var express = require('express'),
     app = express(),
     request = require('request'),
     https = require('https'),
-    http = require('http');
-    
+    http = require('http'),
+    simple_oauth2 = require('simple-oauth2');
 
+// var url =  'https://na30.salesforce.com/services/data',
+//     theHost = 'https://na30.salesforce.com',
+//     thePath = '/services/data';
+/*************************************************/
+
+
+
+
+
+/*************** For setting up the server ******/
 app.set('views', __dirname + '/../views');
 app.set('view engine', 'ejs');
 
-var url =  'https://na30.salesforce.com/services/data',
-    theHost = 'https://na30.salesforce.com',
-    thePath = '/services/data',
-    token;
+// Directs to main page
+app.get('/', function (req, res) {
+    res.render('main');
+});
 
-var options = {
-    host: 'na30.salesforce.com',
-    port: 443,
-    path: '/services/data/v36.0/analytics/reports/00O36000005vYLW/',
-    method: 'GET',
-    headers: {
-        'Authorization': ('Bearer ' + token),
-        'Content-Type': 'application/json'
-    }
-};
+// Directs to data page
+app.get('/data', function (req, res) {
+    res.render('data');
+});
+
+app.listen(process.env.PORT || 5000);
+
+console.log('Express server started on port 5000');
+/************************************************/
+
+
+
+
+
+/* For oauth2 authentication and token handling */
 var credentials = {
-  clientID: 'MVG9uudbyLbNPZOEM.vAy8Y1H8RF8ocpnP1nW2Nt_2a9aFFOjolOIyKa6.1QCCfC9ZreHWPMWEIJhSnQuQqP',
-  clientSecret: '4299800700281945236',
-  site: 'https://login.salesforce.com/services/oauth2/token'
-};
+      clientID: 'MVG9uudbyLbNPZOEM.vAy8Y1H8RF8ocpnP1nW2Nt_2a9aFFOjolOIyKa6.1QCCfC9ZreHWPMWEIJhSnQuQqP',
+      clientSecret: '4299800700281945236',
+      site: 'https://login.salesforce.com/services/oauth2/token'
+    };
+
+var token,
+    tokenConfig = {
+        code: '<code>',
+        redirect_uri: 'http://localhost:3000/callback'
+    };
 
 // Initialize the OAuth2 Library
-var oauth2 = require('simple-oauth2')(credentials);
+var oauth2 = simple_oauth2(credentials);
 
 // Authorization uri definition
 var authorization_uri = oauth2.authCode.authorizeURL({
-  redirect_uri: 'https://hello-world360.herokuapp.com/data',
-  scope: 'full'
-});
+        redirect_uri: 'https://hello-world360.herokuapp.com/data',
+        scope: 'full'
+    });
 
-// Initial page redirecting to Github
+// Initial page redirecting to Salesforce
 app.get('/auth', function (req, res) {
     res.redirect(authorization_uri);
 });
+
+// Promises
+// Save the access token
+oauth2.authCode.getToken(tokenConfig)
+    .then(function saveToken(result) {
+        token = oauth2.accessToken.create(result);
+    })
+    .catch(function logError(error) {
+        console.log('Access Token Error', error.message);
+    });
+/************************************************/
+
+
+
+
+
+/*************** For getting data ***************/
+var options = {
+        host: 'na30.salesforce.com',
+        port: 443,
+        path: '/services/data/v36.0/analytics/reports/00O36000005vYLW/',
+        method: 'GET',
+        headers: {
+            'Authorization': ('Bearer ' + token),
+            'Content-Type': 'application/json'
+        }
+    };
 
 function getJSON(options, onResult){
     console.log("rest::getJSON");
@@ -76,7 +132,7 @@ function getJSON(options, onResult){
     req.end();
 };
 
-// Initial page redirecting to Github
+// Redirect to pull data from Salesforce
 app.get('/getData', function (req, res) {
     getJSON(options,
         function(statusCode, result)
@@ -87,27 +143,33 @@ app.get('/getData', function (req, res) {
             res.send(result);
         });
 });
+/************************************************/
 
-// Callback service parsing the authorization token and asking for the access token
-app.get('/callback', function (req, res) {
-    var code = req.query.code;
-    oauth2.authCode.getToken(tokenConfig).then(function saveToken(result) {
-            token = oauth2.accessToken.create(result);
-    }).catch(function logError(error) {
-        console.log('Access Token Error', error.message);
-    });
-});
 
-app.get('/', function (req, res) {
-  res.render('main');
-});
 
-app.get('/data', function (req, res) {
-  res.render('data');
-});
 
-app.listen(process.env.PORT || 5000);
 
-console.log('Express server started on port 5000');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
