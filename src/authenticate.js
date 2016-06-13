79d85437fd3b637f566c45e956795d874038dd78
Jacob Seibert <12jtseibert@gmail.com>
2016-06-13 15:08:49 -0500

    pulling report data from salesforce report given by Jeff

--- src/authenticate.js
+++ src/authenticate.js
@@ -100,7 +100,7 @@ app.get('/callback', function (req, res) {
         options = {
 	        host: hostURL,
 	        port: 443,
-	        path: '/services/data/v35.0/analytics/reports/',
+	        path: '/services/data/v35.0/analytics/reports//00Oa0000008r7sg/describe',
 	        method: 'GET',
 	        headers: {
 	            'Authorization': 'Bearer ' + token.token.access_token,
