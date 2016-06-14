ce387d2111734fcd426535c84b8128c152f3b186
Jacob Seibert <12jtseibert@gmail.com>
2016-06-14 09:13:26 -0500

    test

--- src/authenticate.js
+++ src/authenticate.js
@@ -18,7 +18,7 @@ var express = require('express'),
     https = require('https'),
     http = require('http'),
     oauth2 = require('simple-oauth2'),
-    jquery = require('jquery');
+    $ = require('jquery');
 
 // var url =  'https://na30.salesforce.com/services/data',
 //     theHost = 'https://na30.salesforce.com',
@@ -129,7 +129,7 @@ var data;
 /*************** For getting data ***************/
 // Redirect to pull data from Salesforce
 app.get('/getData', function (req, res) {
-    jquery.$.ajax({
+    jquery.ajax({
          url: (options.host + options.path),
          data: data,
          type: "GET",
